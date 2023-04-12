95fccb6498a18ed0eb08652cc3c1e66ee1589250
moolasudhakarreddy31 <moolasudhakarreddy8008@gamil.com>
2023-04-12 20:50:39 +0530

    ertyui

---
+++ SecondApp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+
+def wish2(request):
+    return HttpResponse('<h1>Hi This is Second App Aplication</h1>')
